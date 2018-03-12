ea57c1729f2e72daabdd1831fdfe2c0634ebab5a
Raziel <razielmcr1@ciencias.unam.mx>
2018-03-12 14:01:58 -0600

    se agrega .sql

---
+++ SQL/proyecto-servicio-social.sql
@@ -0,0 +1,20 @@
+-- Caso de uso 
+-- 	    Registrar
+
+-- Usando la BD ForoCiencias
+-- y un esquema de la misma, modeloForo
+
+-- Insertando en la tabla usuario
+
+insert into usuario(
+	nombreUsuario,
+	correoCiencias,
+	password,
+	genero,
+	fechaNac
+	)values(
+	val1,
+	val2,
+	val3,
+	val4
+	);
