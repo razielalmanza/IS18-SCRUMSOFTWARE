2035143e1e0e81cffa8f8d8d8522814ce0046a5d
marco.olea <marco.olea@ciencias.unam.mx>
2018-03-13 02:02:24 -0600

    Se mezcló la rama de Marco con la de Jose Luis.

--- SQL/proyecto-servicio-social.sql
+++ SQL/proyecto-servicio-social.sql
@@ -2,36 +2,6 @@
 --   INGENIERIA DE SOFTWARE 2018-2
 -- ================================= --
 
--- --------------------------------- --
---  Caso de uso:
---       Iniciar/Cerrar Sesion
---  Responsable:
---       Jose Luis Vazquez Lazaro
--- --------------------------------- --
-
---  Supongamos que se ha creado una BD
--- llamada ForoCiencias  y un esquema,
--- de esta BD, llamado modeloForo.
-
-drop extension if exists pgcrypto;
-create extension pgcrypto;
-
---  La tabla usuario contendra la info
--- que un  Usuario  de  la  aplicacion
--- registro    para    poder   iniciar 
--- sesion en el sistema.
-drop table if exists modeloForo.usuario;
-
-create table modeloForo.usuario (
-	id serial primary key,
-	nombreUsuario text not null,
-	correoCiencias text not null,
-	password text not null,
-	genero text not null,
-	fechaNac date not null,
-	constraint usuarioUnico unique (usuario)
-);
-
 
 -- --------------------------------- --
 --  Caso de uso:
@@ -89,3 +59,38 @@ COMMENT ON TABLE modeloForo.Respuesta IS
 		'La fecha de creación de la respeusta.';
 	COMMENT ON COLUMN modeloForo.Respuesta.id_pregunta IS 
 		'El identificador único de la pregunta que se está contestando.';
+
+
+
+-- --------------------------------- --
+--  Caso de uso:
+--       Iniciar/Cerrar Sesion
+--  Responsable:
+--       Jose Luis Vazquez Lazaro
+-- --------------------------------- --
+
+--  Supongamos que se ha creado una BD
+-- llamada ForoCiencias  y un esquema,
+-- de esta BD, llamado modeloForo.
+
+drop extension if exists pgcrypto;
+create extension pgcrypto;
+
+--  La tabla usuario contendra la info
+-- que un  Usuario  de  la  aplicacion
+-- registro    para    poder   iniciar 
+-- sesion en el sistema.
+drop table if exists modeloForo.usuario;
+
+create table modeloForo.usuario (
+	id serial primary key,
+	nombreUsuario text not null,
+	correoCiencias text not null,
+	password text not null,
+	genero text not null,
+	fechaNac date not null,
+	constraint usuarioUnico unique (usuario)
+);
+
+
+
