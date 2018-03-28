c2bbe137047dd2f8e1649661131be1248a772db8
LuisLazaro88 <luis_lazaro@ciencias.unam.mx>
2018-03-27 22:00:20 -0600

    Modificación para la aplicaión ForoCiencias

---
+++ ForoCiencias/target/generated-sources/annotations/com/mx/fciencias/scrumsoftware/model/Login_.java
@@ -0,0 +1,15 @@
+package com.mx.fciencias.scrumsoftware.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-27T21:26:21")
+@StaticMetamodel(Login.class)
+public class Login_ { 
+
+    public static volatile SingularAttribute<Login, String> password;
+    public static volatile SingularAttribute<Login, String> usuario;
+    public static volatile SingularAttribute<Login, Integer> id;
+
+}
